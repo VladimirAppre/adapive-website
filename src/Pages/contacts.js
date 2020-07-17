cd73a754f78cc0f621e8d02955710b214d557c49
Vladimir K <vladimir.appre@gmail.com>
2020-07-18 01:06:25 +0300

    contacts

--- src/Pages/contacts.js
+++ src/Pages/contacts.js
@@ -1,11 +1,31 @@
 import React, {Component} from 'react';
+import {Container, Form, Button} from 'react-bootstrap';
 
 class Contacts extends Component {
   render() {
     return (
-      <div>
-        contacts
-      </div>
+      <>
+        <Container style={{width: '500px'}}>
+          <h1 className='text-center'> Свяжитесь со мной!</h1>
+          <Form>
+            <Form.Group controlId='formBasicEmail'>
+              <Form.Label>Email:</Form.Label>
+              <Form.Control type='email' placeholder='Ввести Email'/>
+              <Form.Text className='text-secondary'>
+                Мы никогда не отправим ваш Email третьим лицам
+              </Form.Text>
+            </Form.Group>
+            <Form.Group controlId='formBasicPassword'>
+              <Form.Label>Сообщение:</Form.Label>
+              <Form.Control as='textarea' rows='3'/>
+            </Form.Group>
+            <Form.Group controlId='formBasicCheckbox'>
+              <Form.Check type='checkbox' label='check me out'/>
+            </Form.Group>
+            <Button variant='primary' type='submit'>Отправить</Button>
+          </Form>
+        </Container>
+      </>
     );
   }
 }
