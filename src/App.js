af905d3983fb5b770754c08398a8f0c2721a1ec0
Vladimir K <vladimir.appre@gmail.com>
2020-07-17 18:24:52 +0300

    initial

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import {Button} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <>
+      <h1>Privet</h1>
+      <Button variant="primary">Button</Button>
+    </>
+  )
+}
+
+      export default App;
