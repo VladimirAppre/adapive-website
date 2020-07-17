c9b6c1138888c9d53181775a5e63ca66c9fb2d5a
Vladimir K <vladimir.appre@gmail.com>
2020-07-17 20:00:34 +0300

    routing

--- src/Components/header/header.js
+++ src/Components/header/header.js
@@ -1,46 +1,57 @@
 import React, {Component} from 'react';
 import {Button, Container, Form, FormControl, Nav, Navbar} from "react-bootstrap";
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './header.css';
 import logo from './logo192.png'
+import Home from "../../Pages/home";
+import About from "../../Pages/about";
+import Contacts from "../../Pages/contacts";
+import Blog from "../../Pages/blog";
 
 class Header extends Component {
   render() {
     return (
       <>
-        <div>
-          <Navbar fixed={'top'} collapseOnSelect expand='md' bg='dark' variant='dark'>
-            <Container>
-              <Navbar.Brand href='/'>
-                <img
-                  src={logo}
-                  className='d-inline-block align-top logo'
-                  alt='logo'
-                />React
-              </Navbar.Brand>
-              <Navbar.Toggle aria-controls='responsive-navbar-nav'/>
-              <Navbar.Collapse id='responsive-navbar-nav'>
-                <Nav className='mr-auto'>
-                  <Nav.Link href='/'>Домой</Nav.Link>
-                  <Nav.Link href='/'>О нас</Nav.Link>
-                  <Nav.Link href='/'>Контакты</Nav.Link>
-                  <Nav.Link href='/'>Блог</Nav.Link>
-                </Nav>
-                <Form inline>
-                  <FormControl
-                    type='text'
-                    placeholder='Поиск'
-                    className="mr-sm-2"
-                  />
-                  <Button variant="outline-info">Поиск</Button>
-                </Form>
-              </Navbar.Collapse>
-            </Container>
-          </Navbar>
-        </div>
+        <Navbar  collapseOnSelect expand='md' bg='dark' variant='dark' className='navBar'>
+          <Container>
+            <Navbar.Brand href='/'>
+              <img
+                src={logo}
+                className='d-inline-block align-top logo'
+                alt='logo'
+              />React
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls='responsive-navbar-nav'/>
+            <Navbar.Collapse id='responsive-navbar-nav'>
+              <Nav className='mr-auto'>
+                <Nav.Link href='/'>Домой</Nav.Link>
+                <Nav.Link href='/about'>О нас</Nav.Link>
+                <Nav.Link href='/contacts'>Контакты</Nav.Link>
+                <Nav.Link href='/blog'>Блог</Nav.Link>
+              </Nav>
+              <Form inline>
+                <FormControl
+                  type='text'
+                  placeholder='Поиск'
+                  className="mr-sm-2"
+                />
+                <Button variant="outline-info">Поиск</Button>
+              </Form>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+
+        <Router>
+          <Switch>
+            <Route exact path='/' component={Home}/>
+            <Route exact path='/about' component={About}/>
+            <Route exact path='/contacts' component={Contacts}/>
+            <Route exact path='/blog' component={Blog}/>
+          </Switch>
+        </Router>
       </>
     );
   }
 }
-
 
 export default Header;
