8a8954b89a7e9bd67afb555e8a5c7ce7ca1c2548
Vladimir K <vladimir.appre@gmail.com>
2020-07-20 12:46:16 +0300

    delete excess

--- client/src/index.js
+++ client/src/index.js
@@ -1,28 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import createSagaMiddleware from 'redux-saga'
-import {applyMiddleware, compose, createStore} from 'redux';
-import {Provider} from 'react-redux';
-
 
 import './index.css';
 import App from './App';
-import reducer from "./redux/reducer";
-import * as serviceWorker from './serviceWorker';
-
-const saga = createSagaMiddleware();
-const store = createStore(reducer, compose(
-  applyMiddleware(saga), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-));
-
-// saga.run(sagaWatcher)
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App/>
-  </Provider>,
-
+    <App/>,
   document.getElementById('root')
 );
 
-serviceWorker.unregister();
