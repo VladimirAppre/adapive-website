78c45dc23f4fcb86063c8aad8f8043eacb3bf2db
Vladimir K <vladimir.appre@gmail.com>
2020-07-30 17:26:57 +0300

    connect image list api

--- client/src/item-list/list-image.js
+++ client/src/item-list/list-image.js
@@ -4,30 +4,32 @@ import Carousel from "react-bootstrap/Carousel";
 import ImageStockService from "../mock- services/image-stock-service";
 
 const ImageList = ({data}) => {
-
-  const items = data.map(imgObj => {
-    const {id, url, title, description} = imgObj;
+  if (data) {
+    const items = data.map(imgObj => {
+      const {id, url, title, description} = imgObj;
+      return (
+        <Carousel.Item key={id}>
+          <img
+            className='d-block w-100'
+            src={url}
+            alt='img forest'/>
+          <Carousel.Caption>
+            <h3 className='titleImage'>{title}</h3>
+            <p className='textImage'>{description}</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      )
+    })
     return (
-      <Carousel.Item key={id}>
-        <img
-          className='d-block w-100'
-          src={url}
-          alt='img forest'/>
-        <Carousel.Caption>
-          <h3 className='titleImage'>{title}</h3>
-          <p className='textImage'>{description}</p>
-        </Carousel.Caption>
-      </Carousel.Item>
+      <div>
+        <Carousel>
+          {items}
+        </Carousel>
+      </div>
     )
-  })
+  }
 
-  return (
-    <div>
-      <Carousel>
-        {items}
-      </Carousel>
-    </div>
-  )
+  return null
 }
 
 
@@ -35,29 +37,28 @@ const ImageList = ({data}) => {
 
 const ListImage = () => {
 
-  // const [imageState, setImageState] = useState(null);
-  // const [error, setError] = useState(null)
+  const [imageState, setImageState] = useState(null);
+  const [error, setError] = useState(null)
   const imageStockService = new ImageStockService();
 
-  // useEffect(() => {
-  //
-  //   const fetchData = async () => {
-  //     try {
-  //       const response = await imageStockService.getImages();
-  //       await setImageState(response)
-  //       console.log(imageState)
-  //     } catch (e) {
-  //       setError(e)
-  //     }
-  //   }
-  //   fetchData()
-  //
-  // }, []);
+  const fetchData = async () => {
+    try {
+      const response = await imageStockService.getImages();
+      await setImageState(response)
+    } catch (e) {
+      setError(e)
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, []);
 
   return (
     <>
+      {error && <p>Что то пошло не так...</p>}
       <ImageList
-        data={imageStockService.data}
+        data={imageState}
       />
     </>)
 }
